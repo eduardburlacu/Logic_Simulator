398953da7ac59fb3badbc1f1d12326e3dc81a0fc
eduardburlacu <efb48@cam.ac.uk>
2024-05-12 14:32:43 +0100

    scanner, advance and skip comment to be implemented

--- src/scanner.py
+++ src/scanner.py
@@ -9,15 +9,6 @@ Scanner - reads definition file and translates characters into symbols.
 Symbol - encapsulates a symbol and stores its properties.
 """
 
-SYMBOL_TYPES = ["NAME","DIGIT","PUNCTUATION","END"]
-NAMES_SYMBOLS = ["DEVICES","CONNECTIONS","MONITOR"]
-PUNCTUATION_SYMBOLS = [">",".",",",'"',";","="]
-END_SYMBOL = ""
-KEYWORDS = []
-LETTERS=["a","A","b","B"]
-DIGITS=[f"{i}" for i in range(10)]
-
-
 class Symbol:
 
     """Encapsulate a symbol and store its properties.
@@ -34,6 +25,8 @@ class Symbol:
         """Initialise symbol properties."""
         self.type = None
         self.id = None
+        self.line = None
+        self.line_position = None
 
 
 
@@ -60,44 +53,136 @@ class Scanner:
     def __init__(self, path, names):
         """Open specified file and initialise reserved words and IDs."""
         self.current_character = ""
+
         self.names = names
 
-    def skip_spaces(self):
-        pass
+        self.keywords = [ "DEVICES", "CONNECTIONS", "MONITOR" ]
 
-    def get_name(self):
+        self.digits = [ str(i) for i in range(10) ]
+
+        self.punctuation = [">", ".", ",", '"', ";", "=" ]
+
+        self.symbol_type_list = [
+            self.KEYWORD,self.NAME,self.NUMBER, self.PUNCTUATION,
+            self.EOF, self.GREATER, self.DOT, self.COMMA,
+            self.QUOTE, self.SEMICOL, self.EQUAL
+        ] = range(11)
+
+        self.letters = ["A", "B", "C", "D", "E", "F", "G"
+            , "H", "I", "J", "K", "L", "M", "N"
+            , "O", "P", "Q", "R", "S", "T", "U"
+            , "V", "W", "X", "Y", "Z", "a", "b"
+            , "c", "d", "e", "f", "g", "h", "i"
+            , "j", "k", "l", "m", "n", "o", "p"
+            , "q", "r", "s", "t", "u", "v", "w"
+            , "x", "y", "z"]
+
+        self.end = ""
+
+        # Open the file
+        self.input_file = open( path, "r" )
+
+    def get_next_character(self):
+        """Read and return the next character in input_file."""
+        char = self.input_file.read(1)
+
+        # Enable if you want to get rid of linespaces
+        if char == "\n":
+            char = self.input_file.read(1)
+        return char
+
+    def skip_spaces(self):
+        while True:
+            ch = self.get_next_character()
+            if ch != " " or ch == "":
+                return ch
+
+    def skip_comment(self):
         pass
 
     def get_number(self):
-        pass
+        ch = self.get_next_character()
+        number = ""
+        while ch != "":
+            while ch.isdigit():
+                number = number + ch
+                ch = self.get_next_character()
+            if number != "":
+                break
+            else:
+                ch = self.get_next_character()
+        return [number, ch]
+
+    def get_name(self):
+        ch = self.get_next_character()
+        while not ch.isalpha():
+            ch = self.get_next_character()
+            if ch == "":
+                return [None, ch]
+        name = ""
+        while ch != "":
+            while ch.isalnum():
+                name = name + ch
+                ch = self.get_next_character()
+            if name != "":
+                break
+            else:
+                ch = self.get_next_character()
+        if name == "":
+            name = None
+        return [ name, ch]
 
     def advance(self):
         pass
 
     def get_symbol(self):
         """Translate the next sequence of characters into a symbol."""
+
         symbol = Symbol()
         self.skip_spaces()  # current character now not whitespace
 
         if self.current_character.isalpha():  # name
             name_string = self.get_name()
-            if name_string in self.keywords_list:
+            if name_string in self.keywords:
                 symbol.type = self.KEYWORD
             else:
                 symbol.type = self.NAME
-                [symbol.id] = self.names.lookup([name_string])
+                [symbol.id] = self.names.lookup( [name_string] )
+
+        elif self.current_character == "#":
+            #This is a comment. Ignore this line
+            self.skip_comment()
 
         elif self.current_character.isdigit():  # number
             symbol.id = self.get_number()
             symbol.type = self.NUMBER
 
         elif self.current_character == "=":  # punctuation
-            symbol.type = self.EQUALS
+            symbol.type = self.EQUAL
             self.advance()
 
         elif self.current_character == ",":
             # etc for other punctuation
-            pass
+            symbol.type = self.COMMA
+            self.advance()
+
+        elif self.current_character == ">":
+            # etc for other punctuation
+            symbol.type = self.GREATER
+            self.advance()
+
+        elif self.current_character == ";":
+            # etc for other punctuation
+            symbol.type = self.SEMICOL
+
+
+        elif self.current_character == ".":
+            symbol.type = self.DOT
+            self.advance()
+
+        elif self.current_character =='"':
+            symbol.type = self.QUOTE
+            self.advance()
 
         elif self.current_character == "":  # end of file
             symbol.type = self.EOF
