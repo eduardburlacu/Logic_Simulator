f486c9a1ea1e814f32ae0f8bd8e5f667052c8919
eduardburlacu <efb48@cam.ac.uk>
2024-05-24 02:35:19 +0100

    test pipeline for parser

--- src/test_parse.py
+++ src/test_parse.py
@@ -61,3 +61,33 @@ def test_parse_network(parser):
     print("\n")
     parse = parser.parse_network()
     assert parse is True
+
+def test_parse_all():
+    def claim(l:str):
+        if l.upper()=="T":return True
+        elif l.upper()=="F":return False
+        else:
+            raise RuntimeError("TEST FILE NOT PROPERLY NAMED")
+    outcomes=[]
+    for f in os.listdir(os.path.join(os.path.dirname(__file__),"..", "def_files")):
+        #truth = claim(f[0])
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__),"..","def_files",f))
+        scn = Scanner(
+            path,
+            names=Names(),
+            devices=Names(["CLOCK", "SWITCH", "AND", "NAND", "CLK", "OR", "NOR", "XOR", "DTYPE"]),
+            keywords=Names(["DEVICES", "CONNECTIONS", "MONITORS", "DATA", "SET", "CLEAR", "Q", "QBAR", "I"]),
+            punct=Names([",", ".", ":", ";", ">", "[", "]", "="])
+        )
+        parser = Parser(
+            names=Names(),
+            devices=None,
+            network=None,
+            monitors=None,
+            scanner=scn,
+        )
+        parse = parser.parse_network()
+        #assert parse == truth
+        #outcomes.append(parse)
+
+    print(outcomes)
